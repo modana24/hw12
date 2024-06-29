b1b2548beafa9b6c542cfd9e02c92c2aeee5fc7d
dasha <dasha.motry2002@gmail.ru>
2024-06-29 23:11:50 +0300

    готово

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) {
+        Author author = new Author("Ахмет","Алтан");
+        Book book = new Book("Мадам Хаят", author,2023);
 
+        Author author2 = new Author("Анна","Красильщик");
+        Book book2 = new Book("Давай поедем в Уналашку", author2,2022);
+
+        System.out.println(book.getYear());
+        book.setYear(2025);
+        System.out.println(book.getYear());
     }
 }
